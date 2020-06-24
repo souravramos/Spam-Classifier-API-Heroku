251b248f2f45b275e77d3be30ffbf4fa83dc2989
souravramos <souravramos04@gmail.com>
2020-06-24 12:48:14 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, url_for, request
+import pandas as pd 
+import pickle
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.externals import joblib
+
+filename = 'nlp_model.pkl'
+clf = pickle.load(open(filename, 'rb'))
+cv = pickle.load(open('transform.pkl', 'rb'))
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('home.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+#	df = pd.read_csv('spam.csv', encoding='latin-1')
+#	df.drop(['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'], axis=1, inplace=True)
+#
+#	# features and labels
+#	df['label'] = df['class'].map({'ham': 0, 'spam': 1})
+#	X = df['message']
+#	y = df['label']
+#
+#	# extract feature with CountVectorizer
+#	cv = CountVectorizer()
+#	X = cv.fit_transform(X) # fit the data
+
+#	pickle.dump(cv, open('transform.pkl', 'wb'))
+#
+#	from sklearn.model_selection import train_test_split
+#	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+#
+#	# Naive Bayes Classifier
+#	clf = MultinomialNB()
+#	clf.fit(X_train, y_train)
+#	clf.score(X_test, y_test)
+#	filename = 'nlp_model.pkl'
+#	pickle.dump(clf, open(filename, 'wb'))	
+
+
+	if request.method == 'POST':
+		message = request.form['message']
+		data = [message]
+		vect = cv.transform(data).toarray()
+		my_prediction = clf.predict(vect)
+	return render_template('result.html', prediction=my_prediction)
+
+
+if __name__ == '__main__':
+	app.run(debug=True)	
